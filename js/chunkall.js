7abf10a4f6224f2e9144aa1881f52d4cb9eb23fc
Zhang,Junming <junming.zhang@netscout.com>
2021-03-06 22:28:54 +0800

    update

--- js/chunkall.js
+++ js/chunkall.js
@@ -2489,8 +2489,8 @@ setInterval(async ()=>{
                     {
                         if(ret.result)
                         {
-                        vue_pets.myGemsCnt = ret.retobj.gemCnt;
-                        vue_pets.myPetsCnt = ret.retobj.petCnt;
+                        vue_pets.myGemsCnt = big2number(ret.retobj.gemCnt);
+                        vue_pets.myPetsCnt = big2number(ret.retobj.petCnt);
                         }
 
                     });
