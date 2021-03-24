2887e374759edf715561a9d5c0c44872886e95dc
Bright <voteit.top@hotmail.com>
2021-03-24 02:28:42 +0000

    update

--- js/chunkall.js
+++ js/chunkall.js
@@ -3148,7 +3148,7 @@ uefa_vue = new Vue(
                            itemObj.uvotes = big2number(ret.retobj.uVotes)/DECIMALS;
                            itemObj.expire = big2number(ret.retobj.expireTs); 
                            itemObj.ltStr = uefa_vue.leftTime(itemObj.expire);
-                           this.groups[i].items[j].ltStr = itemObj.ltStr;
+                           uefa_vue.groups[i].items[j].ltStr = itemObj.ltStr;
                            const timestamp = (Date.now()/1000).toFixed(0);
                            itemObj.active = timestamp > itemObj.expire;
                         }	
